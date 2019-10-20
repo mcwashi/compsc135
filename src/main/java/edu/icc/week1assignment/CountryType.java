04c021219be945941c81d475f53a1c05a038b4d8
Marcus Washington <marcus.washington@me.com>
2019-10-20 01:20:48 -0500

    Moving on Down

--- src/main/java/edu/icc/week1assignment/CountryType.java
+++ src/main/java/edu/icc/week1assignment/CountryType.java
@@ -7,8 +7,88 @@ package edu.icc.week1assignment;
 
 /**
  *
- * @author SucramMedia
+ * @author Marcus Washington
  */
 public class CountryType {
+    
+    private String capital;
+    private String country;
+    private String continent;
+    private int population;
+    private String language;
+
+    /**
+     * @return the capital
+     */
+    public String getCapital() {
+        return capital;
+    }
+
+    /**
+     * @param capital the capital to set
+     */
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the continent
+     */
+    public String getContinent() {
+        return continent;
+    }
+
+    /**
+     * @param continent the continent to set
+     */
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    /**
+     * @return the population
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * @param population the population to set
+     */
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    /**
+     * @return the language
+     */
+    public String getLanguage() {
+        return language;
+    }
+
+    /**
+     * @param language the language to set
+     */
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    
+    
+    
+ 
     
 }
---
+++ src/main/java/edu/icc/week1assignment/CountryTypeImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.icc.week1assignment;
+
+/**
+ *
+ * @author SucramMedia
+ */
+public class CountryTypeImpl {
+    
+    
+    public static void main(String args[]){
+        CountryType obj[] = new CountryType[2];
+    }
+    
+}
